e5eb63a7ab533c9895e27435d01b162c7ff9b258
Vika Malykh <vika.malykh.05@bk.ru>
2025-05-02 17:23:45 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,90 @@
+const books = [
+    {
+        author: 'Виктор Пелевин',
+        Name: 'Путешествие в Элевсин',
+        publicationYear: 2023,
+        price: 650,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Валя Филиппенко',
+        Name: 'Миссия не сахар',
+        publicationYear: 2024,
+        price: 480,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Станислав Богдановский',
+        Name: 'Сделай Так, и Всё получится!',
+        publicationYear: 2025,
+        price: 720,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Джон Грин',
+        Name: 'Виноваты звёзды',
+        publicationYear: 2012,
+        price: 350,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Харуки Мураками',
+        Name: 'Норвежский лес',
+        publicationYear: 1987,
+        price: 500,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Дэн Браун',
+        Name: 'Код да Винчи',
+        publicationYear: 2003,
+        price: 600,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Холли Блэк',
+        Name: 'Жестокий принц',
+        publicationYear: 2018,
+        price: 450,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Джейн Остин',
+        Name: 'Гордость и предубеждение',
+        publicationYear: 1813,
+        price: 300,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Эрих Мария Ремарк',
+        Name: 'Три товарища',
+        publicationYear: 1936,
+        price: 550,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Кристофер Паолини',
+        Name: 'Эрагон',
+        publicationYear: 2002,
+        price: 800,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Рэй Брэдбери',
+        Name: '451 градус по Фаренгейту',
+        publicationYear: 1953,
+        price: 400,
+        bestseller: true,
+        available: true
+    },
+];
